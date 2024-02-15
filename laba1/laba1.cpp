c71ea3e5bd22f8502ce7434d4c15c1fc5e9d5278
NNI <89315757+KenjIsanl@users.noreply.github.com>
2024-02-16 01:51:01 +0400

    Update laba1.cpp

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -29,24 +29,48 @@ using namespace std;
 // 	cout << "*:" << p << " - " << *p << '\n';
 // } // Разыменовка 
 
-struct GAME
-{
-    int win;
-    int defeat;
-    int draw;
+// struct GAME
+// {
+//     int win;
+//     int defeat;
+//     int draw;
 
-};
+// };
 
-int main(void) {
-    struct GAME p;
-    p.win = 4;
-    p.defeat = 2;
-    p.draw = 6;
-    cout << "GAME win: " << p.win << endl;
-    cout << "GAME defeat: " << p.defeat << endl;
-    cout << "GAME draw: " << p.draw << endl;
+// int main(void) {
+//     struct GAME p;
+//     p.win = 4;
+//     p.defeat = 2;
+//     p.draw = 6;
+//     cout << "GAME win: " << p.win << endl;
+//     cout << "GAME defeat: " << p.defeat << endl;
+//     cout << "GAME draw: " << p.draw << endl;
     
-    return 0;
+//     return 0;
 
-}
+// } //структура пример
+
+// struct NPC_vec3 {
+// 	float x, y, z;
+// 	int armor;
+// 	int HP;
+
+// };
+
+// int main() {
+// 	NPC_vec3 g;
+// 	g.x = 3.1;
+// 	g.y = 7;
+// 	g.z = 8;
+// 	g.HP = 440;
+// 	g.armor = 10;
+// 	int c = g.x + g.y + g.z ;
+// 	cout << c << std::endl;
+	
+
+// 	cout << "NPS 1: " << g.x << " KM " << std::endl;
+// }
+
+
+
 
